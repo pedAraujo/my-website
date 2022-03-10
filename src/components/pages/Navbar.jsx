5589b6652e2c12e566ddef427b6515b528c3aa2c
ceduardo <cs.eduardo@icloud.com>
2022-03-10 09:45:50 -0300

    Some Fixes

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { MdFingerprint } from "react-icons/md";
-import { FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar() {
   return (
@@ -9,7 +7,6 @@ function Navbar() {
       <div className="navbar">
         <div className="navbar-container container">
           <Link to="/" className="navbar-logo">
-            <MdFingerprint className="navbar-icon" />
             PEDRO
           </Link>
           <div className="menu-icon">{}</div>
