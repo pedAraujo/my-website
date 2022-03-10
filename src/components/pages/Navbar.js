2f914344ccd6b1aede4bc237d4aa04197b2bc5c7
Pedro <p3d.araujo@icloud.com>
2022-03-09 23:42:21 -0300

    welcome again

--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
         <div className="navbar-container container">
           <Link to="/" className="navbar-logo">
             <MdFingerprint className="navbar-icon" />
-            HELLO INTERNET :)
+            PEDRO
           </Link>
           <div className="menu-icon">{}</div>
         </div>
